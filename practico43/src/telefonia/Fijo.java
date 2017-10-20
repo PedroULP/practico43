eb9839451ae82202ad67ab30ed7f6e0155bc8003
Piedri <Piedri@10.62.200.184>
2017-10-20 19:41:08 -0300

    Se creo la clase Fijo

---
+++ practico43/src/telefonia/Fijo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telefonia;
+
+/**
+ *
+ * @author Piedri
+ */
+public class Fijo extends Telefono {
+    
+        private String tipoDeLinea;
+        
+        public Fijo (long numero, String modelo, String marca, String tipoDeLinea){
+            super(numero,modelo,marca);
+            this.tipoDeLinea = tipoDeLinea;
+        }
+        
+        public boolean llamar(long nroDestino){
+        System.out.println("Estoy llamando a "+nroDestino + "...");
+        return true;
+        }
+
+    public String getTipoDeLinea() {
+        return tipoDeLinea;
+    }
+
+    public void setTipoDeLinea(String tipoDeLinea) {
+        this.tipoDeLinea = tipoDeLinea;
+    }
+        
+        
+    
+}
