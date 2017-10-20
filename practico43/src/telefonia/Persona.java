7a1debde0ce2970f69a2ab26f7417caeb7c8ac6c
Piedri <Piedri@10.62.200.184>
2017-10-20 19:33:48 -0300

    Se creo clase Persona (errores debido a no crear todavia clase Celular)

---
+++ practico43/src/telefonia/Persona.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telefonia;
+
+/**
+ *
+ * @author Piedri
+ */
+public class Persona {
+    
+        private long dni;
+        private String nombre;
+        
+        
+        public Persona(long dni, String nombre){
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+        
+        public void llamarMensajear (Telefono telefono, long nroDestino, String mensaje)
+        {
+            if (telefono instaceof Celular)
+            {
+                Celular c1 = (Celular) telefono;
+                cl.enviarMensaje(nroDestino,mensaje);
+            }
+            else 
+            {
+                System.out.println("Solo los celulares pueden enviar mensajes");
+            }
+        }
+        
+        public void llamarMensajear (Telefono telefono, long nroDestino)
+        {
+            telefono.llamar(nroDestino);
+        }
+    
+}
