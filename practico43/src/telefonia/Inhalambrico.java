9b6fca210eb106414ab6c0178bdf39681860ca40
claudio <claudio@claudio-Lenovo-G40-80>
2017-10-20 19:32:50 -0300

    clase inhalambrico

---
+++ practico43/src/telefonia/Inhalambrico.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telefonia;
+
+/**
+ *
+ * @author claudio
+ */
+public class Inhalambrico extends Telefono{
+    private int canal;
+    private double alcance;
+    
+    //Constructor
+    public Inhalambrico(long numero,String modelo,String marca,int canal,double alcance){
+        super(numero,modelo,marca);
+        this.canal=canal;
+        this.alcance=alcance;
+    }
+
+    /**
+     * @return the canal
+     */
+    public int getCanal() {
+        return canal;
+    }
+
+    /**
+     * @param canal the canal to set
+     */
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    /**
+     * @return the alcance
+     */
+    public double getAlcance() {
+        return alcance;
+    }
+
+    /**
+     * @param alcance the alcance to set
+     */
+    public void setAlcance(double alcance) {
+        this.alcance = alcance;
+    }
+
+    public boolean llamar(long nroDestino){
+        System.out.println("Estoy llamando a "+nroDestino);
+        return true;
+    }
+    
+        
+}
